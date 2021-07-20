df43212d4d7559d5171aea9815c9a8d31394d25a
Hamidou TESSILIMI <hamidtessilimi@gmail.com>
2021-07-20 12:45:45 +0100

    feat: home component

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,54 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, ActivityIndicator, FlatList, Image, TouchableWithoutFeedback } from 'react-native';
 
 export default function Home() {
+    const [dataLoading, finishLoading] = useState(true);
+    const [newsData, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://newsapi.org/v2/everything?q=tech&apiKey=f7a3b20ae6a24a0a8e694769c01995ca')
+            .then((response) => response.json())
+            .then((json) => setData(json.articles))
+            .catch((error) => console.log(error))
+            .finally(() => finishLoading(false))
+    }, []);
+
+    const storyItem = ({item}) => {
+        return (
+            <TouchableWithoutFeedback
+                onPress={() =>
+                    navigation.navigate("NewsDetail", {url: item.url})
+                }
+            >
+
+                <View style={styles.listings}>
+                    <Text style={styles.title}>
+                        {item.title}
+                    </Text>
+
+                    <Image style={styles.img}
+                        source={{
+                            uri: item.urlToImage
+                        }}
+                    />
+                    <Text style={styles.desc}>
+                        {item.description}
+                    </Text>
+                 </View>
+
+            </TouchableWithoutFeedback>
+        );
+    };
+
     return (
         <View style={styles.container}>
-            <Text>My Home component</Text>
+            {dataLoading ? <ActivityIndicator /> : (
+                <FlatList
+                    data={newsData}
+                    renderItem={storyItem}
+                    keyExtractor={(item) => item.url}
+                />
+            )}
         </View>
     );
 };
@@ -15,5 +59,26 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        width: '100%',
+        padding: 20
+    },
+    img: {
+        height: 100,
+        width: '98%',
+    },
+    listings: {
+        paddingTop: 15,
+        paddingBottom: 25,
+        borderBottomColor: 'black',
+        borderBottomWidth: 1
+    },
+    title: {
+        paddingBottom: 10,
+        fontFamily: 'OpenSans',
+        fontWeight: 'bold',
+    },
+    desc: {
+        fontFamily: 'OpenSans',
+        fontStyle: 'italic'
     }
 })
